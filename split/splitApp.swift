ca9ecfa7637b510268d26c513d5dc623487506a9
chrissy <chrissy.chiang@wemoscooter.com>
2025-12-06 17:43:40 +0800

    Initial Commit

---
+++ split/splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitApp.swift
+//  split
+//
+//  Created by Chrissy on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
